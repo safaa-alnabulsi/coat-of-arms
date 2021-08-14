2d6c15700681408283cb252d207639a8269484db
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2021-08-14 14:45:17 +0200

    Add more objects and modifieres

--- src/alphabet.py
+++ src/alphabet.py
@@ -1,6 +1,9 @@
 class Alphabet:
     def __init__(self):
-        self.color = ['A', 'B', 'O', 'S', 'G', 'E', 'X']
-        self.object = ['lion', 'fess', 'cross', 'bend', 'chief']
-        self.modifier = ['rampant', 'dancetty', 'salient', 'checky']
-        self.position = ['acc.', 'ch.', 'in', 'of']
+        self.color = ['A', 'B', 'O', 'S', 'G', 'E', 'X', 'V', 'Z']
+        self.object = ['lion', 'fess', 'cross', 'crescent', 'chief', 'chevron', 'escutch', 'escutcheon', 'mullet',
+                       'eagle', 'bars', 'heads', 'fleur-de-lis', 'martlets', 'saltire', 'estoile', 'roses', 'head',
+                       'leaves', 'fish']
+        self.modifier = ['rampant', 'dancetty', 'salient', 'checky', 'roundel', 'bend', 'annulet', 'lozenge',
+                         'engrailed', 'orle', 'crusily', 'pale']
+        self.position = ['acc.', 'ch.', 'in', 'of', '&', 'and', 'sn', 'dx', 'nebuly', 'dancetty']
