d98b134aadf45249940756e7541b5b8a8d345fb5
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2021-08-15 22:00:00 +0200

    Support possessive pronouns and plurals

--- src/alphabet.py
+++ src/alphabet.py
@@ -2,17 +2,20 @@ class Alphabet:
     def __init__(self):
         self.color = ['A', 'B', 'O', 'S', 'G', 'E', 'X', 'V', 'Z']
         self.object = ['lion', 'fess', 'cross', 'crescent', 'chief', 'chevron', 'escutch', 'escutcheon', 'mullet',
-                       'eagle', 'bars', 'heads', 'fleur-de-lis', 'martlets', 'saltire', 'estoile', 'roses', 'head',
-                       'leaves', 'fish', 'leopard', 'bear', 'ram', 'lamb', 'boar', 'boars', 'cows', 'duck', 'dragon',
-                       'dragons', "dragon's", 'merlette', 'cock', 'volcano', 'falcon', 'wing', 'wings', 'oak',
-                       'fleurs-de-lis', 'branch',
-                       'dices']
+                       'eagle', 'bars', 'fleur-de-lis', 'martlet', 'saltire', 'estoile', 'rose', 'head',
+                       'leave', 'fish', 'leopard', 'bear', 'ram', 'lamb', 'boar', 'cow', 'duck',
+                       'dragon', 'merlette', 'cock', 'volcano', 'falcon', 'wing', 'oak',
+                       'fleurs-de-lis', 'branch', 'bull', 'elephant', 'griffin', 'horseshoe', 'hare',
+                       'panther', 'hand', 'bugle-horn', 'lure', 'tail',
+                       'dice', 'donkey', 'face', 'unicorn', 'blackbird', 'marmite', 'attire']
 
         self.modifier = ['rampant', 'dancetty', 'salient', 'checky', 'roundel', 'bend', 'annulet', 'lozenge',
                          'engrailed', 'orle', 'crusily', 'pale', 'doubleheaded', 'rising', 'addorsed', 'slipped',
-                         'erect', 'cr.', 'border', 'chained']
+                         'erect', 'cr.', 'border', 'chained', 'flory', 'erased', 'hooded', 'winged', 'embattled',
+                         'gorged', 'arched', 'segreant']
 
-        self.position = ['acc.', 'ch.', 'in', 'of', '&', 'and', 'sn', 'dx', 'nebuly', 'dancetty', 'above', 'with'
+        self.position = ['acc.', 'ch.', 'in', 'of', '&', 'and', 'sn', 'dx', 'nebuly', 'dancetty', 'above', 'with',
+                         'betw'
                          # 'pd',
                          # 'isst', 'jesst', 'passt', 'guard', 'sejt', 'reguard', 'inv', 'cch', 'compony',
                          # 'segr', 'p.c.', 'p.n.', 'col', 'displayed', 'indented', 'towards', 'chf',
