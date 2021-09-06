b9c80a746222cdda17f78bd4cb8b4a6dc3237755
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2021-09-06 16:10:02 +0200

    Add functionality to get valid lables of a charge

--- src/label_checker_automata.py
+++ src/label_checker_automata.py
@@ -7,18 +7,34 @@ class LabelCheckerAutomata:
                  objects=alphabet.OBJECTS,
                  modifiers=alphabet.MODIFIERS,
                  positions=alphabet.POSITIONS,
-                 numbers=alphabet.NUMBERS):
+                 numbers=alphabet.NUMBERS, support_plural=True):
+        
+        if support_plural == True:
+            transitions=  {
+                    'q0': {'c': 'q1', 'p': 'q0', 'o': 'q0', 'm': 'q0', 'n': 'q0'},
+                    'q1': {'c': 'q1', 'o': 'q2', 'm': 'q1', 'p': 'q1', 'n': 'q5'},
+                    'q5': {'c': 'q5', 'o': 'q2', 'm': 'q5', 'p': 'q5', 'n': 'q5'},
+                    'q2': {'o': 'q2', 'm': 'q3', 'p': 'q4', 'c': 'q2', 'n': 'q5'},
+                    'q3': {'m': 'q3', 'o': 'q2', 'p': 'q4', 'c': 'q3', 'n': 'q5'},
+                    'q4': {'p': 'q4', 'o': 'q2', 'm': 'q3', 'c': 'q4', 'n': 'q4'},
+                }
+            states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5'}
+            input_symbols = {'c', 'o', 'm', 'p', 'n'}
+        else: # no numbers
+            transitions = {
+                    'q0': {'c': 'q1', 'p': 'q0', 'o': 'q0', 'm': 'q0'},
+                    'q1': {'c': 'q1', 'o': 'q2', 'm': 'q1', 'p': 'q1'},
+                    'q2': {'o': 'q2', 'm': 'q3', 'p': 'q4', 'c': 'q2'},
+                    'q3': {'m': 'q3', 'o': 'q2', 'p': 'q4', 'c': 'q3'},
+                    'q4': {'p': 'q4', 'o': 'q2', 'm': 'q3', 'c': 'q4'},
+            }
+            states = {'q0', 'q1', 'q2', 'q3', 'q4'}
+            input_symbols = {'c', 'o', 'm', 'p'}
+        
         self.dfa = DFA(
-            states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5'},
-            input_symbols={'c', 'o', 'm', 'p', 'n'},  # c: color, o: object, m: modifier, p: position, n: number
-            transitions={
-                'q0': {'c': 'q1', 'p': 'q0', 'o': 'q0', 'm': 'q0', 'n': 'q0'},
-                'q1': {'c': 'q1', 'o': 'q2', 'm': 'q1', 'p': 'q1', 'n': 'q5'},
-                'q5': {'c': 'q5', 'o': 'q2', 'm': 'q5', 'p': 'q5', 'n': 'q5'},
-                'q2': {'o': 'q2', 'm': 'q3', 'p': 'q4', 'c': 'q2', 'n': 'q5'},
-                'q3': {'m': 'q3', 'o': 'q2', 'p': 'q4', 'c': 'q3', 'n': 'q5'},
-                'q4': {'p': 'q4', 'o': 'q2', 'm': 'q3', 'c': 'q4', 'n': 'q4'},
-            },
+            states=states,
+            input_symbols=input_symbols,  # c: color, o: object, m: modifier, p: position, n: number
+            transitions=transitions,
             initial_state='q0',
             final_states={'q2', 'q3'}
         )
@@ -94,6 +110,23 @@ class LabelCheckerAutomata:
                 output['positions'].append(elem)
 
         return output
+    
+    def get_valid_labels(self, all_labels):
+        validated_labels = {}
+        for label in all_labels:
+            validated_labels[label] = self.is_valid(label)
+            
+        return validated_labels
+    
+
+    def get_valid_labels_of(self, all_labels, charge):
+        validated_labels = self.get_valid_labels(all_labels)
+        charge_labels = []
+        for l, valid in validated_labels.items():
+            if valid and charge in l: 
+                charge_labels.append(l)
+        
+        return charge_labels
         
     
     @staticmethod
