e599f30594f13809109f517c2802264be0b9bc26
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2021-11-14 21:05:29 +0100

    Add use-case when there is no modifier

--- src/armoria_api.py
+++ src/armoria_api.py
@@ -53,11 +53,16 @@ class ArmoriaAPIPayload:
     def __init__(self, struc_label, position='e', scale='1.5'):
         self.position = position
         self.scale = scale
-        
+        print(struc_label)
         shield_color = struc_label['shield']['color']
         charge_color = struc_label['objects'][0]['color'] # for now, only first charge is considered
         charge = struc_label['objects'][0]['charge']
-        first_modifier = struc_label['objects'][0]['modifiers'][0]
+        try:
+            first_modifier = ' ' + struc_label['objects'][0]['modifiers'][0]
+        except IndexError:
+            first_modifier = ''
+
+        
         
         try:
             self.api_shield_color = COLORS_MAP[shield_color]
@@ -66,7 +71,7 @@ class ArmoriaAPIPayload:
             raise ValueError('Invalid color')
         
         try:
-            key = charge + ' ' + first_modifier
+            key = charge + first_modifier
             self.api_charge = MODIFIERS_MAP[key]
         except KeyError:
             raise ValueError('Invalid charge')
--- tests/test_armoria_api.py
+++ tests/test_armoria_api.py
@@ -4,8 +4,8 @@ from src.caption import Caption
 
 
 pairs_label_payload = {
-# 'B A lion': {'t1': 'azure', 'shield': 'heater', 'charges': [{'charge': 'lionRampant', 't': 'argent', 'p': 'e', 'size': '1.5'}]},
-# 'B A eagle': {'t1': 'azure', 'shield': 'heater', 'charges': [{'charge': 'eagle', 't': 'argent', 'p': 'e', 'size': '1.5'}]},
+'B A lion': {'t1': 'azure', 'shield': 'heater', 'charges': [{'charge': 'lionRampant', 't': 'argent', 'p': 'e', 'size': '1.5'}]},
+'B A eagle': {'t1': 'azure', 'shield': 'heater', 'charges': [{'charge': 'eagle', 't': 'argent', 'p': 'e', 'size': '1.5'}]},
 'B A eagle doubleheaded': {'t1': 'azure', 'shield': 'heater', 'charges': [{'charge': 'eagleTwoHeards', 't': 'argent', 'p': 'e', 'size': '1.5'}]},
 }
 
