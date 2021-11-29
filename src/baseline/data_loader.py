45bd054ac5c39acaca7e0881f5b42cf879a97f84
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2021-11-29 15:17:55 +0000

    Refactor dataloader

--- src/baseline/data_loader.py
+++ src/baseline/data_loader.py
@@ -3,15 +3,15 @@ from src.baseline.vocabulary import Vocabulary
 from src.baseline.coa_dataset import CoADataset
 from src.baseline.caps_collate import CapsCollate
 
-def get_loader(root_folder, train_annotation_file, val_annotation_file, test_annotation_file, transform, batch_size=32, num_workers=2, shuffle=True, pin_memory=True, vocab=None):
+
+def get_loader(root_folder, annotation_file, transform, 
+               batch_size=32, num_workers=2, shuffle=True, pin_memory=True, vocab=None):
     
-    train_dataset = CoADataset(root_folder, train_annotation_file, transform=transform, vocab=vocab)
-    val_dataset = CoADataset(root_folder, val_annotation_file, transform=transform, vocab=vocab)
-    test_dataset = CoADataset(root_folder, test_annotation_file, transform=transform, vocab=vocab)
-    pad_idx = train_dataset.vocab.stoi["<PAD>"]
+    dataset = CoADataset(root_folder, annotation_file, transform=transform, vocab=vocab)
+    pad_idx = dataset.vocab.stoi["<PAD>"]
 
-    train_loader = DataLoader(
-        dataset=train_dataset,
+    loader = DataLoader(
+        dataset=dataset,
         batch_size=batch_size,
         num_workers=num_workers,
         shuffle=shuffle,
@@ -19,22 +19,16 @@ def get_loader(root_folder, train_annotation_file, val_annotation_file, test_ann
         collate_fn=CapsCollate(pad_idx=pad_idx)
     )
 
-    val_loader = DataLoader(
-        dataset=val_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=shuffle,
-        pin_memory=pin_memory,
-        collate_fn=CapsCollate(pad_idx=pad_idx)
-    )
+    return loader, dataset
 
-    test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=shuffle,
-        pin_memory=pin_memory,
-        collate_fn=CapsCollate(pad_idx=pad_idx)
-    )
+def get_loaders(root_folder, train_annotation_file, val_annotation_file, test_annotation_file, 
+                transform, batch_size=32, num_workers=2, shuffle=True, pin_memory=True, vocab=None):
+    
+    train_loader, train_dataset = get_loader(root_folder, train_annotation_file, transform, 
+                                             batch_size, num_workers,shuffle, pin_memory, vocab)
+    val_loader, val_dataset = get_loader(root_folder, val_annotation_file, transform, 
+                                         batch_size, num_workers,shuffle, pin_memory, vocab)
+    test_loader, test_dataset = get_loader(root_folder, test_annotation_file, transform, 
+                                           batch_size, num_workers,shuffle, pin_memory, vocab)
 
     return train_loader, val_loader, test_loader, train_dataset, val_dataset, test_dataset
