e700d387369f51501b9898441b747708c6d54901
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2021-12-06 10:30:32 +0100

    train on the whole dataset

--- train_baseline.py
+++ train_baseline.py
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
     
-    data_location = '/home/space/datasets/COA/generated-data-api'
-#     data_location = '/home/space/datasets/COA/generated-data-api-small'
+    #data_location = '/home/space/datasets/COA/generated-data-api'
+    data_location = '/home/space/datasets/COA/generated-data-api-small'
 
     
     caption_file = data_location + '/captions.txt'
