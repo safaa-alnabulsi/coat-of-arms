9ec938210feb4969df031e4f53cdf04de76ae12a
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2022-04-04 10:15:23 +0000

    Fix testing the baseline model

--- src/baseline/coa_model.py
+++ src/baseline/coa_model.py
@@ -213,14 +213,14 @@ def save_model(model, optimizer, loss, accuracy, model_full_path, hyper_params):
 
 def load_model(model_path, hyper_params, learning_rate, drop_prob, ignored_idx):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model, optimizer, _ = get_new_model(hyper_params, learning_rate, ignored_idx, drop_prob, device)
+    model, optimizer, criterion = get_new_model(hyper_params, learning_rate, ignored_idx, drop_prob, device)
         
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     loss = checkpoint['loss']
 
-    return model, optimizer, loss
+    return model, optimizer, loss,criterion
 
 def load_model_checkpoint(model_path, hyper_params, learning_rate, drop_prob, ignored_idx):    
     model, optimizer, criterion = get_new_model(hyper_params, learning_rate, ignored_idx, drop_prob, device)
