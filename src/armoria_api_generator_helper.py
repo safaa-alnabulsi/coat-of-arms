c0f20440f23c2ddb5956259bebef430d641fc7ef
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2022-04-10 17:46:31 +0200

    Refactor the code of generting pixels of images into api-gen-helper

--- src/armoria_api_generator_helper.py
+++ src/armoria_api_generator_helper.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
+from PIL import Image
+import torchvision.transforms as T
 from src.caption import Caption
 from src.armoria_api import ArmoriaAPIPayload, ArmoriaAPIWrapper
 
@@ -16,7 +19,7 @@ class ArmoriaAPIGeneratorHelper:
             text_label = ' '.join(label).strip()
             sample_name = 'image_' + str(i)
 
-            self.write_image_label_to_file(
+            self._write_line_to_file(
                 self.caption_file, sample_name + '.png,' + text_label)
 
     def generate_dataset(self):
@@ -58,14 +61,40 @@ class ArmoriaAPIGeneratorHelper:
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-    def creat_caption_file(self):
-        f = open(self.caption_file, "w+")
-        f.write('image,caption')
+    def creat_caption_file(self, filename,columns='image,caption'):
+        f = open(filename, "w+")
+        f.write(columns)
         f.write('\n')
         f.close()
 
-    def write_image_label_to_file(self, filename, line):
+    def _write_line_to_file(self, filename, line):
         with open(filename, 'a') as f:
             f.write(line)
             f.write('\n')
+        f.close()
+
+    def _calc_img_pixels(self, image_full_path):
+        my_image = Path(image_full_path)
+        if not my_image.exists():
+            print(f'skipping image {image_full_path}, as it does not exist')
+
+        img = Image.open(image_full_path).convert("RGB")
+        trans = T.ToTensor()
+        img = trans(img)
+        return img.sum()
+
+    def add_pixels_column(self, root_folder, new_caption_file,old_caption_file, start_index=1):
+        with open(old_caption_file, 'r') as f:
+            garbage=[next(f) for i in range(0,start_index+1)]  
+            for line in f:
+                if 'image,caption' in line:
+                    continue
+                    
+                image_name, text_label = line.strip().split(',')
+                image_full_path = root_folder + '/images/' + image_name
+                img_pixels = self._calc_img_pixels(image_full_path)
+                newline = f'{image_name},{text_label},{img_pixels}'
+
+                self._write_line_to_file(new_caption_file, newline)
+
         f.close()
