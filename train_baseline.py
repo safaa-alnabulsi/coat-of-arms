46650e025636528a224313462fddb60b00abb475
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2022-11-14 14:45:07 +0100

    comment out the seed for now

--- train_baseline.py
+++ train_baseline.py
@@ -56,14 +56,14 @@ if __name__ == "__main__":
     # ---------------------- Reproducibility -------------------
     
     seed = args.seed
-    random.seed(seed)     # python random generator
-    np.random.seed(seed)  # numpy random generator
+    # random.seed(seed)     # python random generator
+    # np.random.seed(seed)  # numpy random generator
 
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed_all(seed)
 
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
     
     # ----------------------------------------------------------------- 
 
